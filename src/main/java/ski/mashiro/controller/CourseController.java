1782c2604ab155e776e01b00ad315c1a6d457add
FeczIne <mashirotj@gmail.com>
2022-10-01 21:17:46 +0800

    -

--- src/main/java/ski/mashiro/controller/CourseController.java
+++ src/main/java/ski/mashiro/controller/CourseController.java
@@ -1,7 +1,10 @@
 package ski.mashiro.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ski.mashiro.pojo.Course;
+import ski.mashiro.pojo.Result;
+import ski.mashiro.service.CourseService;
 
 /**
  * @author FeczIne
@@ -9,5 +12,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/schedules")
 public class CourseController {
-
+    private final CourseService courseService;
+    @Autowired
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+    @PostMapping
+    public Result saveCourse(Course course) {
+        return courseService.saveCourse(course);
+    }
+    @DeleteMapping
+    public Result deleteCourse(String courseName) {
+        return courseService.deleteByCourseName(courseName);
+    }
+    @PutMapping
+    public Result updateCourse(Course course) {
+        return courseService.updateCourse(course);
+    }
+    @GetMapping("/courseName/{courseName}")
+    public Result getCourseByCourseName(@PathVariable String courseName) {
+        return courseService.getCourseByCourseName(courseName);
+    }
+    @GetMapping("/courseDate/{courseDate}")
+    public Result listAllByCourseDateCourses(@PathVariable String courseDate) {
+        return courseService.listAllByCourseDateCourses(courseDate);
+    }
+    @GetMapping
+    public Result listAllCourses() {
+        return courseService.listAllCourses();
+    }
 }
