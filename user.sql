5fe31b7c8d720888d91248853ea045ff53071a13
mashitot <mashirotj@gmail.com>
2022-10-12 22:31:18 +0800

    完善api，修复bug

--- user.sql
+++ user.sql
@@ -1,34 +1,10 @@
--- --------------------------------------------------------
--- 主机:                           sh-cynosdbmysql-grp-eauf5d9w.sql.tencentcdb.com
--- 服务器版本:                        5.7.18-cynos-log - 20200531
--- 服务器操作系统:                      Linux
--- HeidiSQL 版本:                  12.1.0.6537
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
--- 导出  表 project.user 结构
 CREATE TABLE IF NOT EXISTS `user` (
-  `user_code` varchar(30) NOT NULL,
-  `user_password` char(32) NOT NULL,
-  `password_salt` char(50) NOT NULL,
-  `user_nickname` varchar(12) DEFAULT NULL,
-  `user_term_initial_date` date NOT NULL,
-  `user_table_name` varchar(33) NOT NULL,
-  PRIMARY KEY (`user_code`)
+    `user_code` varchar(30) NOT NULL,
+    `user_password` char(32) NOT NULL,
+    `password_salt` char(50) NOT NULL,
+    `user_nickname` varchar(12) DEFAULT NULL,
+    `user_term_initial_date` date NOT NULL,
+    `user_table_name` varchar(33) NOT NULL,
+    `user_api_token` char(32) NOT NULL,
+    PRIMARY KEY (`user_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 数据导出被取消选择。
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
