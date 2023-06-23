b962da8fea26666f47d6405dff0e42cf82ea32b3
mashirot <mashirotj@gmail.com>
2023-06-24 02:37:45 +0800

    debug

--- src/test/java/ski/mashiro/ScheduleApplicationTests.java
+++ src/test/java/ski/mashiro/ScheduleApplicationTests.java
@@ -1,12 +1,13 @@
 package ski.mashiro;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.threeten.extra.Days;
 import org.threeten.extra.Weeks;
+import ski.mashiro.util.JwtUtils;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 @SpringBootTest
 class ScheduleApplicationTests {
@@ -18,5 +19,11 @@ class ScheduleApplicationTests {
         int betweenWeek = Weeks.between(date, now).getAmount();
         int betweenDay = Days.between(date, now).getAmount();
         System.out.println(betweenDay % 7 == 0 ? betweenWeek : betweenWeek + 1);
+    }
+
+    @Test
+    void testJwt() {
+        String token = JwtUtils.createToken("mashiro");
+        Assertions.assertTrue(JwtUtils.verifyToken(token, "mashiro"));
     }
 }
